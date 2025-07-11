98d693a2f251e5d9eaa07a7926737181c39d7556
Fadua El Kourti <f.elkourti@prodware.es>
2025-07-11 07:53:59 +0200

    Update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const {createRequestHandler} = require('@remix-run/express');
 const express = require('express');
 const path = require('path');
 
-const BUILD_DIR = path.join(process.cwd(), 'dist'); // Ajusta si tu build sale en otra carpeta
+const BUILD_DIR = path.join(process.cwd(), 'dist', 'server'); // o 'dist' según tu build
 const app = express();
 
 app.use(express.static('public'));
